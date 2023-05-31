69edf4ee14b88230cb899601f54397ad8c6a4c4d
bemtorres <benja.mora.torres@gmail.com>
2023-05-31 13:35:39 -0400

    inicio proyecto

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+/**
+ * @param String name
+ * @return String
+ */
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function getParams() {
+    // let params = new URLSearchParams(location.search);
+    // var contract = params.get('contrato');
+}
